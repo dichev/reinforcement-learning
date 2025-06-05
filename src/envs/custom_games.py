f4b6093518fdb8bbd3698028df44bd42c3beb8a6
dichev <g.dichev@gmail.com>
2025-06-05 16:22:24 -0700

    Custom FrozenLake

---
+++ src/envs/custom_games.py
@@ -0,0 +1,26 @@
+import gymnasium as gym
+from gymnasium.envs.registration import register
+from envs.wrappers import StepPenaltyWrapper, DiscreteOneHotWrapper, DiscountedRewardWrapper
+from lib.playground import play_episode
+
+
+def make__FrozenLake_OneHot_StepPenalty(render_mode=None, is_slippery=False, gamma=0.99):
+    env = gym.make("FrozenLake-v1", render_mode=render_mode, is_slippery=is_slippery)
+    env = DiscountedRewardWrapper(env, gamma=gamma)
+    env = DiscreteOneHotWrapper(env)
+    return env
+
+
+env_id = 'custom/FrozenLake-OneHot-StepPenalty'
+if env_id not in gym.envs.registry:
+    register(id=env_id, entry_point='src.envs.custom_games:make__FrozenLake_OneHot_StepPenalty')
+
+
+
+# Testing only
+if __name__ == '__main__':
+    env = gym.make('custom/FrozenLake-OneHot-StepPenalty', render_mode='human')
+    episode = play_episode(env, policy=lambda obs: env.action_space.sample())
+    print(episode)
+    env.close()
+
---
+++ src/envs/wrappers.py
@@ -0,0 +1,59 @@
+import numpy as np
+import gymnasium as gym
+
+
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    """
+    Converts discrete state to one-hot vector.
+    """
+
+    def __init__(self, env):
+        super(DiscreteOneHotWrapper, self).__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        self.observation_space = gym.spaces.Box(0.0, 1.0, (env.observation_space.n, ), dtype=np.float32)
+
+    def observation(self, state):
+        onehot = np.zeros(self.observation_space.shape, dtype=np.float32)
+        onehot[state] = 1.0
+        return onehot
+
+
+class DiscountedRewardWrapper(gym.RewardWrapper):
+    """
+    Applies a discount factor to the reward at each step based on
+    """
+
+    def __init__(self, env, gamma=0.99):
+        super(DiscountedRewardWrapper, self).__init__(env)
+        self.gamma = gamma
+        self._steps = 0
+
+    def reset(self, **kwargs):
+        self._steps = 0
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        discounted_reward = (self.gamma ** self._steps) * reward
+        self._steps += 1
+        return obs, discounted_reward, terminated, truncated, info
+
+
+
+class StepPenaltyWrapper(gym.Wrapper):
+    """
+    Subtracts a small penalty at each step.
+    """
+
+    def __init__(self, env, step_penalty=-0.01):
+        super(StepPenaltyWrapper, self).__init__(env)
+        self.step_penalty = step_penalty
+
+    def step(self, action):
+        state, reward, terminated, truncated, info = self.env.step(action)
+        if not (terminated or truncated):
+            reward = self.step_penalty
+
+        return state, reward, terminated, truncated, info
+
+
