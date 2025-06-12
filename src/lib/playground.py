a77611b6c6ea54ba20ef1a4b3d37b3d0588963c9
dichev <g.dichev@gmail.com>
2025-06-11 17:24:48 -0700

    Support trajectories

--- src/lib/playground.py
+++ src/lib/playground.py
@@ -3,9 +3,10 @@ import numpy as np
 
 class Episode:
     def __init__(self):
-        self.observations = []
         self.actions = []
         self.rewards = []
+        self.observations = []
+        self.term_observation = None
         self.total_rewards = 0
         self.steps = 0
         self.done = False
@@ -17,12 +18,21 @@ class Episode:
         self.total_rewards += reward
         self.steps += 1
 
-    def finish(self, as_tensors=True):
+    def finish(self, term_obs, as_tensors=True):
+        self.term_observation = term_obs
         self.done = True
         if as_tensors:
             self.observations = torch.tensor(np.vstack(self.observations))
             self.actions = torch.tensor(self.actions)
             self.rewards = torch.tensor(self.rewards)
+
+    def as_trajectory(self):
+        for i in range(self.steps):
+            obs = self.observations[i]
+            action = self.actions[i]
+            reward = self.rewards[i]
+            obs_next = self.observations[i + 1] if i < len(self.observations) - 1 else self.term_observation
+            yield obs, action, reward, obs_next
 
 
     def __repr__(self):
@@ -45,7 +55,17 @@ def play_episode(env, policy, as_tensors=True):
         obs_next, reward, terminated, truncated, _ = env.step(action)
         episode.step(obs, action, reward)
         if terminated or truncated:
-            episode.finish(as_tensors)
+            episode.finish(obs_next, as_tensors)
             return episode
 
         obs = obs_next
+
+
+if __name__ == '__main__':
+    import gymnasium as gym
+    env = gym.make("CartPole-v1", render_mode="human")
+    episode = play_episode(env, lambda obs: env.action_space.sample())
+    print(f"Episode finished with reward {episode.total_rewards}")
+    trajectory = list(episode.as_trajectory())
+    print(trajectory)
+    env.close()
