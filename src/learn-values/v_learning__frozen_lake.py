717a6d764ecf311cc2d96bbba03da69c1ff0b0e5
dichev <g.dichev@gmail.com>
2025-06-11 18:31:34 -0700

    Compare value learning vs action-value learning

--- src/learn-values/v_learning__frozen_lake.py
+++ src/learn-values/v_learning__frozen_lake.py
@@ -10,78 +10,111 @@ TEST_EPISODES = 20
 GAMMA = .9
 
 
-class Agent:
+class AgentV:
     def __init__(self, k_actions, n_states, gamma):
         self.k_actions = k_actions
         self.n_states = n_states
+        self.gamma = gamma
         self.memory = {
             'values':      np.zeros((n_states,)),
-            'q_values':    np.zeros((n_states, k_actions)),
             'rewards':     np.zeros((n_states, k_actions, n_states)),
             'transitions': np.zeros((n_states, k_actions, n_states)),  # number of transitions
         }
-        self.gamma = gamma
 
     def collect(self, state, action, reward, state_next):
         self.memory['rewards'][state, action, state_next] = reward  # note: average it for stochastic rewards
         self.memory['transitions'][state, action, state_next] += 1
 
-    def update_action_values(self):
-        Q = [[self.calc_actions_values(state, action) for action in range(self.k_actions)] for state in range(self.n_states)]
-        Q = np.array(Q)
+    def calc_actions_values(self, state):
+        Q = np.zeros(self.k_actions)
+        for action in range(self.k_actions):
+            counts = self.memory['transitions'][state, action]
+            total = counts.sum()
+
+            # skip states with no transitions
+            filtered_states = np.where(counts > 0)[0]
+
+            # average over future states
+            for i, state_next in enumerate(filtered_states):
+                p = counts[state_next] / total
+                r = self.memory['rewards'][state, action, state_next]
+                v_next = self.get_state_value(state_next)
+                Q[action] += p * (r + self.gamma * v_next)
+
+        return Q
+
+    def get_state_value(self, state):
+        v = self.memory['values'][state]
+        return v
+
+    def update_values(self):
+        Q = np.array([self.calc_actions_values(state) for state in range(self.n_states)])
+
+        # synchronously update all the q values
+        for state in range(self.n_states):
+            self.memory['values'][state] = Q[state].max()
+
+    def policy(self, state):
+        Q = self.calc_actions_values(state) # one step look ahead
+        return np.argmax(Q).item()
+
+    def __repr__(self):
+        return f'AgentV(gamma={self.gamma})'
+
+
+class AgentQ(AgentV): # for comparison train only the q-values
+
+    def __init__(self, k_actions, n_states, gamma):
+        super().__init__(k_actions, n_states, gamma)
+        self.memory['q_values'] = np.zeros((n_states, k_actions))
+        del self.memory['values']
+
+    def get_state_value(self, state):
+        a = self.policy(state)
+        q = self.memory['q_values'][state, a]
+        return q
+
+    def update_values(self):
+        Q = np.array([self.calc_actions_values(state) for state in range(self.n_states)])
+
         # synchronously update all the q values
         self.memory['q_values'][:] = Q
-
-    def calc_actions_values(self, state, action):
-        counts = self.memory['transitions'][state, action]
-        total = counts.sum()
-
-        # skip states with no transitions
-        filtered_states = np.where(counts > 0)[0]
-
-        # average over future states
-        q = 0.
-        for i, state_next in enumerate(filtered_states):
-            p = counts[state_next] / total
-            r = self.memory['rewards'][state, action, state_next]
-            a = self.policy(state_next)
-            q_next = self.memory['q_values'][state_next, a]
-            q += p * (r + self.gamma * q_next)
-
-        return q
 
     def policy(self, state):
         Q = self.memory['q_values'][state]
         return np.argmax(Q).item()
 
     def __repr__(self):
-        return f'Agent(gamma={self.gamma})'
+        return f'AgentQ(gamma={self.gamma})'
 
 
-
-# Test two agents
+# Test agents
 env = gym.make(ENV, render_mode=None)
-agent = Agent(env.action_space.n, env.observation_space.n, gamma=GAMMA)
-
-avg_rewards = []
-for i in range(1, MAX_ITERATIONS + 1):
-
-    # learning
-    for obs, action, reward, obs_next in play_random(env, LEARNING_RANDOM_STEPS):
-        agent.collect(obs, action, reward, obs_next)
-    agent.update_action_values()
-
-    # testing
-    episodes = [play_episode(env, agent.policy, False) for _ in range(TEST_EPISODES)]
-    for e in episodes:
-        for obs, action, reward, obs_next in e.as_trajectory():
+agents = (
+    AgentV(env.action_space.n, env.observation_space.n, gamma=GAMMA),
+    AgentQ(env.action_space.n, env.observation_space.n, gamma=GAMMA),
+)
+for agent in agents:
+    env.reset()
+    avg_rewards = []
+    for i in range(1, MAX_ITERATIONS + 1):
+        # learning
+        for obs, action, reward, obs_next in play_random(env, LEARNING_RANDOM_STEPS):
             agent.collect(obs, action, reward, obs_next)
+        agent.update_values()
 
-    avg_reward = sum([ep.total_rewards for ep in episodes]) / TEST_EPISODES
-    avg_rewards.append(avg_reward)
-    print(f'{i:>5}/{MAX_ITERATIONS}) {agent} | avg_rewards={avg_reward:.2f}')
+        # testing
+        episodes = [play_episode(env, agent.policy, False) for _ in range(TEST_EPISODES)]
+        for e in episodes:
+            for obs, action, reward, obs_next in e.as_trajectory():
+                agent.collect(obs, action, reward, obs_next)
 
-plt.plot(avg_rewards, label=f'{agent}')
+        avg_reward = sum([ep.total_rewards for ep in episodes]) / TEST_EPISODES
+        avg_rewards.append(avg_reward)
+        if i % 10 == 0:
+            print(f'{i:>5}/{MAX_ITERATIONS}) {agent} | avg_rewards={avg_reward:.2f}')
+
+    plt.plot(avg_rewards, label=f'{agent}')
 plt.title('Avg rewards over time')
 optimal_reward = 0.8
 plt.hlines(optimal_reward, 0, MAX_ITERATIONS, color="green", linestyles='dashed')
@@ -94,23 +127,8 @@ plt.show()
 # Play one episode with visualization
 print(f"Playing one episode with the trained agent")
 env = gym.make(ENV, render_mode='human')
-episode = play_episode(env, agent.policy)
+episode = play_episode(env, agents[0].policy)
 print(f"Episode finished with reward {episode.total_rewards}")
 env.close()
 
-
-# For analysis
-analyze = {
-    'Q': agent.memory['q_values'].T,
-    'N': agent.memory['transitions'].sum(axis=0),
-}
-fig = plt.figure(figsize=np.array([env.observation_space.n * 2, env.action_space.n]).clip(6, 20))
-for i, (k, v) in enumerate(analyze.items()):
-    plt.subplot(1, 2, i + 1)
-    plt.title(k)
-    plt.imshow(v, cmap='Blues')
-    plt.xlabel('states')
-    plt.ylabel('actions')
-plt.tight_layout()
-plt.show()
 
