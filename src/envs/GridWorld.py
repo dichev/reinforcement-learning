bc9a24c342459320b085e7d2fc54fa4a59e736f2
dichev <g.dichev@gmail.com>
2025-06-21 18:54:27 -0700

    Add GridWorld custom env

---
+++ src/envs/GridWorld.py
@@ -0,0 +1,43 @@
+import gymnasium as gym
+from gymnasium.envs.registration import register
+from minigrid.wrappers import ImgObsWrapper, FullyObsWrapper
+from lib.playground import play_episode
+
+
+
+class FilterObservations(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Box)
+        space = env.observation_space
+        self.observation_space = gym.spaces.Box(0, 255, (space.shape[0], space.shape[1]), space.dtype)
+
+    def observation(self, observation):
+        objects, colors, state = observation[:, :, 0], observation[:, :, 1], observation[:, :, 2]
+        return objects
+
+
+
+def make__GridWorld(render_mode=None):
+    env = gym.make("MiniGrid-LavaCrossingS9N3-v0", render_mode=render_mode)
+    env = FullyObsWrapper(env)     # full observability
+    env = ImgObsWrapper(env)       # Get rid of the 'mission' field
+    env = FilterObservations(env)  # Filter only objects data
+    return env
+
+
+
+env_id = 'custom/GridWorld'
+if env_id not in gym.envs.registry:
+    register(id=env_id, entry_point='src.envs.GridWorld:make__GridWorld')
+
+
+
+# Testing only
+if __name__ == '__main__':
+    env = gym.make('custom/GridWorld', render_mode='human')
+    episode = play_episode(env, policy=lambda obs: env.action_space.sample())
+    print(episode)
+    print(episode.observations[-1])
+    env.close()
+
