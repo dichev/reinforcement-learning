6dee17e31b40a53bfade96cd75dad4db51df9c57
dichev <g.dichev@gmail.com>
2025-06-28 13:57:25 -0700

    Calc discounted n-step returns

--- src/lib/calc.py
+++ src/lib/calc.py
@@ -1,33 +1,68 @@
 import torch
+import torch.nn.functional as F
 
 
 @torch.jit.script
-def discount_returns_precise(rewards, gamma: float):
+def discount_returns_loop(rewards, gamma: float):
     assert rewards.dtype == torch.float, f"Expected float reward but got {rewards.dtype}"
     T, B = rewards.shape
 
     returns = torch.empty_like(rewards)
-    G = torch.tensor(0., device=rewards.device)
+    G = torch.tensor(0., device=rewards.device, dtype=torch.float)
     for t in range(T-1, -1, -1): # reversed(range(T)) but jit friendly
         G = rewards[t, :] + gamma * G
         returns[t, :] = G
     return returns
 
 
+@torch.jit.script
+def discount_n_steps_loop(rewards, gamma: float, n: int):  # ignores rewards past n future steps
+    assert rewards.dtype == torch.float, f"Expected float reward but got {rewards.dtype}"
+    T, B = rewards.shape
+
+    discounts = gamma ** torch.arange(n, device=rewards.device, dtype=torch.float).view(n, 1)
+    returns = torch.empty_like(rewards)
+    for t in range(T):  # G_t = R_{t+1} + γ R_{t+2} + ... γ^{n-1} R_{t+n}
+        end = min(t + n, T)
+        returns[t] = (rewards[t:end] * discounts[:end - t]).sum(dim=0)
+
+    return returns
+
+
+
 def discount_returns(rewards, gamma):
     assert rewards.dtype == torch.float, f"Expected float reward but got {rewards.dtype}"
     if gamma == 0: return rewards.clone()
     T, B = rewards.shape
-    assert gamma ** T > torch.finfo(torch.float).tiny, "Zero discount detected due to precision issues, use discount_returns_precise"
+    assert gamma ** T > torch.finfo(torch.float).tiny, "Zero discount detected due to precision issues, use discount_returns_loop instead"
 
-    discounts = gamma ** torch.arange(T, device=rewards.device).view(T, 1)
+    discounts = gamma ** torch.arange(T, device=rewards.device, dtype=torch.float).view(T, 1)
     R = rewards * discounts
     G = R.flip(0).cumsum(dim=0).flip(0) / discounts
     return G
 
 
+def discount_n_steps(rewards, gamma, n):  # ignores rewards past n future steps
+    assert rewards.dtype == torch.float, f"Expected float reward but got {rewards.dtype}"
+    T, B = rewards.shape
+    n = min(n, T)
+
+    discounts = gamma ** torch.arange(n, device=rewards.device, dtype=torch.float)   # (n,)  [1, γ, γ², ...]
+    kernel = discounts.view(1, 1, n)                   # (C_out=1, C_in=1, W=n)
+    x = F.pad(rewards.T.view(B, 1, T), pad=(0, n-1))   # (B, C_in=1, W=T+n-1)
+    y = F.conv1d(x, kernel)                            # (B, C_out=1, T)
+    returns = y.squeeze(1).T                           # (T, B)
+    return returns
+
+
+
+
+
 if __name__ == '__main__':
     import timeit
-    print('Precise:   ', timeit.timeit(lambda : discount_returns_precise(torch.randn(1000, 2), .99), number=100, globals=globals()))
-    print('Vectorized:', timeit.timeit(lambda : discount_returns(torch.randn(1000, 2), .99), number=100, globals=globals()))
+    print('Discount Precise:   ', timeit.timeit(lambda : discount_returns_loop(torch.randn(1000, 2), .99), number=100, globals=globals()))
+    print('Discount Vectorized:', timeit.timeit(lambda : discount_returns(torch.randn(1000, 2), .99), number=100, globals=globals()))
+    print('-------------------------------------------------------------')
+    print('N-step discount Loop:      ', timeit.timeit(lambda : discount_n_steps_loop(torch.randn(1000, 2), .99, n=100), number=100, globals=globals()))
+    print('N-step discount Vectorized:', timeit.timeit(lambda : discount_n_steps(torch.randn(1000, 2), .99, n=100), number=100, globals=globals()))
 
--- test/test_calc.py
+++ test/test_calc.py
@@ -1,19 +1,44 @@
 import pytest
 import torch
-from lib.calc import discount_returns_precise, discount_returns
+from lib.calc import discount_returns_loop, discount_returns, discount_n_steps_loop, discount_n_steps
+
+
+@pytest.fixture(params=[
+    torch.ones(10, 1).float(),
+    torch.ones(10, 2).float(),
+    torch.arange(30).view(-1, 1).float(),
+    torch.randn(30, 2).float(),
+], ids=["ones", "ones_batch", "arange", "randn"])
+def rewards(request):
+    return request.param
 
 
 
-@pytest.mark.parametrize("gamma", [0.0, 0.1, 0.9, 0.99, 1.0])
-@pytest.mark.parametrize("rewards", [torch.arange(30).view(-1, 1).float(), torch.randn(30, 2)])
+@pytest.mark.parametrize("gamma", [0, .1, .9, .99, 1])
 def test_discount_functions_match(gamma, rewards):
-    A = discount_returns_precise(rewards, gamma)
+    A = discount_returns_loop(rewards, gamma)
     B = discount_returns(rewards, gamma)
 
-    assert torch.allclose(A, B, atol=1e-5)
+    torch.testing.assert_close(A, B)
     if gamma == 0:
-        assert torch.allclose(A, rewards.float())
-        assert torch.allclose(B, rewards.float())
+        torch.testing.assert_close(A, rewards)
+        torch.testing.assert_close(B, rewards)
     elif gamma == 1:
-        assert torch.allclose(A, rewards.flip(0).cumsum(dim=0).flip(0).float())
-        assert torch.allclose(B, rewards.flip(0).cumsum(dim=0).flip(0).float())
+        torch.testing.assert_close(A, rewards.flip(0).cumsum(dim=0).flip(0).float())
+        torch.testing.assert_close(B, rewards.flip(0).cumsum(dim=0).flip(0).float())
+
+
+@pytest.mark.parametrize("gamma", [0, .1, .9, .99, 1])
+@pytest.mark.parametrize("n", [1, 5, 10, 30, 50])
+def test_discount_functions_match(gamma, n, rewards):
+    A = discount_n_steps_loop(rewards, gamma, n)
+    B = discount_n_steps(rewards, gamma, n)
+
+    torch.testing.assert_close(A, B)
+    if gamma == 0:
+        torch.testing.assert_close(A, rewards)
+        torch.testing.assert_close(B, rewards)
+    elif n >= len(rewards):
+        C = discount_returns_loop(rewards, gamma)
+        torch.testing.assert_close(A, C)
+        torch.testing.assert_close(B, C)
