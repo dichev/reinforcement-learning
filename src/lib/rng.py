dbaf9821564c4d2ba2890176cafeb417dced9df7
dichev <g.dichev@gmail.com>
2025-08-01 16:01:26 -0700

    Support RNG seeding

---
+++ src/lib/rng.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+import random
+
+
+def seed_global(seed: int, deterministic=False):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        torch.use_deterministic_algorithms(True)
